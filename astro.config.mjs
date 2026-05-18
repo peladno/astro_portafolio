ee5df84b4e5937ea19a7effec4fbb46ca1658542
Javier <j.perezurrutia@gmail.com>
2026-05-18 21:47:08 +0900

    starting again the code

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  },
+  integrations: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+});
